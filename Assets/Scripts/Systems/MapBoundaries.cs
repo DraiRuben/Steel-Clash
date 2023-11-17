2d61b3e3376c2359fdcd0c5162c14feb1fc06fc9
DraiRuben <r.drai@lahorde.school>
2023-11-17 12:31:33 +0100

    Folder & MapBoundaries fix

--- Assets/Scripts/Systems/MapBoundaries.cs
+++ Assets/Scripts/Systems/MapBoundaries.cs
@@ -3,12 +3,12 @@ using UnityEngine.InputSystem;
 
 public class MapBoundaries : MonoBehaviour
 {
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D _collider)
     {
         // If any player goes outside the view of the camera
-        if(!collision.isTrigger && collision.CompareTag("Player"))
+        if(!_collider.isTrigger && _collider.CompareTag("Player"))
         {
-            PlayerHealth _health = collision.transform.root.GetComponent<PlayerHealth>();
+            PlayerHealth _health = _collider.transform.root.GetComponent<PlayerHealth>();
 
             // Play the death SFX
             SoundEffectHandler.Instance.PlaySoundEffect(SoundEffectHandler.SoundEffectEnum.death);
@@ -16,14 +16,14 @@ public class MapBoundaries : MonoBehaviour
             // The player will not lose a life when he is alone
             if (PlayerInputManager.instance.playerCount < 2)
             {
-                SpawnManager.instance.PutPlayerAtSpawnPoint(5, collision.transform.root.gameObject);
+                SpawnManager.Instance.PutPlayerAtSpawnPoint(5, _collider.transform.root.gameObject);
                 _health.Percentage = 0;
             }
             else
             {
                 _health.Percentage = 0;
                 _health.Lives--;
-                SpawnManager.instance.PutPlayerAtSpawnPoint(5, collision.transform.root.gameObject);
+                SpawnManager.Instance.PutPlayerAtSpawnPoint(5, _collider.transform.root.gameObject);
             }
         }
     }
